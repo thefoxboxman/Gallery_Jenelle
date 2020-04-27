7d7bbd15f93ee3f14d060f826b2e24a565757cc4
John <john@John-New-MacPro.local>
2020-04-27 14:16:07 +1000

    Navbar fixed to top of page with title

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -5,7 +5,10 @@ import styled from "styled-components"
 const Navbar = () => {
   return (
     <NavWrapper>
-      <nav className="navbar">
+      <nav className="navbar column">
+        <div class="siteTitle">
+          <h1>Jenelle's Photo Gallery</h1>
+        </div>
         <ul>
           <li>
             <Link to="/" className="nav-link">
@@ -59,16 +62,33 @@ export default Navbar
 //style the navbar with media query
 const NavWrapper = styled.div`
   .navbar {
+    position: fixed;
+    top: 0;
+    width: 100%;
     padding-top: 10px;
     border-top: solid 0.1rem white;
     border-bottom: solid 0.1rem white;
+    background-color: black;
+  }
+  .column {
+    display: flex;
+    flex-direction: column;
+  }
+  .siteTitle {
+    margin: 0 auto;
+  }
+  .siteTitle h1 {
+    font-size: 1rem;
+    letter-spacing: 0.5em;
+    font-style: italic;
+    margin: 0.5em 0 1em 0;
   }
   ul {
     display: flex;
     list-style-type: none;
     margin: 0 auto;
     padding: 0;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
   }
 
@@ -79,7 +99,7 @@ const NavWrapper = styled.div`
     border: solid 0.1rem white;
     border-radius: 0.2rem;
     font-family: Roboto, "Open Sans", "Helvetica Neue", sans-serif;
-    font-size: 0.5rem;
+    font-size: 0.75rem;
     padding: 0.1rem 0.3rem;
     margin: 0.2rem;
     text-transform: capitalize;
@@ -88,14 +108,14 @@ const NavWrapper = styled.div`
   /*Mid range style the navbar with media query*/
   @media screen and (min-width: 600px) {
     .nav-link {
-      font-size: 0.75rem;
+      font-size: 1.1rem;
       padding: 0.1rem 0.3rem;
       margin: 1rem;
     }
   }
 
   .nav-link:hover {
-    font-size: 0.75rem;
+    font-size: 1.1rem;
     background: white;
     color: black;
     cursor: pointer;
