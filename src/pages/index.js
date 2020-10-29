0862602a5b59ae68af395e4abec66df1c5c55efd
John <john@John-New-MacPro.local>
2020-10-30 06:28:57 +1000

    Increase number of images deplayed to 200

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import styled from "styled-components"
 
 export const query = graphql`
   {
-    allSanityPhotos(limit: 100, sort: { fields: datetime, order: DESC }) {
+    allSanityPhotos(limit: 200, sort: { fields: datetime, order: DESC }) {
       edges {
         node {
           id
